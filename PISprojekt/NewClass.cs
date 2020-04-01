606d05f7c59c2fafa65154ba00c1d276d4b0b2fd
IvanKalinic <ivankalinic1998@gmail.com>
2020-04-01 18:25:50 +0200

    Merged

--- PISprojekt/NewClass.cs
+++ PISprojekt/NewClass.cs
@@ -4,14 +4,16 @@ namespace PISprojekt
 {
     class NewClass
     {
-<<<<<<< HEAD
+
         public NewClass()
         {
             Console.WriteLine("Nova promjena");
 
+            Console.WriteLine("Promjena");
+
         }
-=======
-        Console.WriteLine("Nova promjena");
->>>>>>> 58ecfa80b9c24a54049561579e417495221959a4
+
+
+
     }
 }
